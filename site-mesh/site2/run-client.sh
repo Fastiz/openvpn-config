a842ba098450c37dbee486b940731aabf418df09
fastiz <fastiz@itba.edu.ar>
2021-06-10 11:23:21 -0300

    fixed site mesh

--- site-mesh/site2/run-client.sh
+++ site-mesh/site2/run-client.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 1 ]
 then
-	sudo openvpn --remote $1 1194 --cert ../client-key-crt/sitio2.crt --key ../client-key-crt/sitio2.key --config client.conf
+	sudo openvpn --remote $1 1194 --cert ../../client-key-crt/sitio2.crt --key ../../client-key-crt/sitio2.key --config client.conf
 else
 	echo "run-client <public ip of site 3 router>"
 fi
--- site-mesh/site3/run-client.sh
+++ site-mesh/site3/run-client.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 1 ]
 then
-	sudo openvpn --remote $1 1194 --cert ../client-key-crt/sitio3.crt --key ../client-key-crt/sitio3.key --config client.conf
+	sudo openvpn --remote $1 1194 --cert ../../client-key-crt/sitio3.crt --key ../../client-key-crt/sitio3.key --config client.conf
 else
 	echo "run-client <public ip of site 1 router>"
 fi
