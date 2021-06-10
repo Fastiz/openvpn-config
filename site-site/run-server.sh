2fb598635e3e7fb5d15ec108eb34c988733c5557
Fastiz <fastiz@itba.edu.ar>
2021-06-10 00:04:58 -0300

    site to site

---
+++ site-site/run-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 2 ]
+then
+	rm ./ccd/*
+	cp ./ccd_template ./ccd/sitio1
+	sed -i "s/<local subnet>/$2/" ./ccd/sitio1 
+	sudo openvpn --push "route $1 255.255.255.0" --config server.conf --route $2 255.255.255.0
+else
+	echo "run-server <local subnet> <remote subnet>"
+fi
+
